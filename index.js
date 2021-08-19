8978f29396eea7d12cc000c7a2ad436c07fb43b0
ajayJayapalan <ajai.jayapalan@gmail.com>
2021-08-19 08:49:19 +0530

    Finished

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require("./startup/route")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
+require("./startup/prod")(app);
 
 const server = app.listen(3000, () => console.log("Listening in http://localhost:3000/api/"));
 
