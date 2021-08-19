bf385638391c54678e6937b8513fdc52adc42050
ajayJayapalan <ajai.jayapalan@gmail.com>
2021-08-19 12:59:11 +0530

    END

--- routes/users.js
+++ routes/users.js
@@ -15,7 +15,6 @@ router.get("/me", auth, async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  console.log("hi")
   const { error } = validateUser(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
