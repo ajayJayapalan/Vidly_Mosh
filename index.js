a51c667567eba790137e06604fed19e884c81892
ajayJayapalan <ajai.jayapalan@gmail.com>
2021-08-19 12:49:51 +0530

    Fixing bugs 2

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require("./startup/config")();
 require("./startup/validation")();
 require("./startup/prod")(app);
 
-const PORT = config.get("PORT") || 3000;
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () =>
   console.log(`Listening in http://localhost:${PORT}/api/`)
 );
